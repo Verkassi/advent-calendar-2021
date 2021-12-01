46d4947a28df54bc0b89da45d5cf003c1988bf87
Stefan Hendriks <stefan.hendriks@ns.nl>
2021-12-01 08:52:29 +0100

    ran pre-commit for the first time

--- solutions/december_1/puzzle_1.py
+++ solutions/december_1/puzzle_1.py
@@ -1,19 +1,19 @@
 # Calculate the number of increases and decreases.
 from puzzle_input import measurement_report, test_report
 
-
 TOTAL_EQUAL = 0
 TOTAL_INCREASE = 0
 TOTAL_DECREASE = 0
 
 
-def calc_diff(previous_measure: int,
-              current_measure: int,
-              total_equal: int = TOTAL_EQUAL,
-              total_increases: int = TOTAL_INCREASE,
-              total_decreases: int = TOTAL_DECREASE,
-              debug: bool = False
-              ) -> None:
+def calc_diff(
+    previous_measure: int,
+    current_measure: int,
+    total_equal: int = TOTAL_EQUAL,
+    total_increases: int = TOTAL_INCREASE,
+    total_decreases: int = TOTAL_DECREASE,
+    debug: bool = False,
+) -> None:
     global TOTAL_EQUAL
     global TOTAL_INCREASE
     global TOTAL_DECREASE
@@ -39,8 +39,11 @@ for i in range(len(report)):
     if i == 0:
         None  # First entry knows no diff
     else:
-        calc_diff(current_measure=measurement_report[i],
-                  previous_measure=measurement_report[i-1]
+        calc_diff(
+            current_measure=measurement_report[i],
+            previous_measure=measurement_report[i - 1],
         )
 
-print(f"Entries: {len(report)}, Number of increases: {TOTAL_INCREASE}, number of decreases: {TOTAL_DECREASE}, number of equals: {TOTAL_EQUAL}")
+print(
+    f"Entries: {len(report)}, Number of increases: {TOTAL_INCREASE}, number of decreases: {TOTAL_DECREASE}, number of equals: {TOTAL_EQUAL}"
+)
--- solutions/december_1/puzzle_2.py
+++ solutions/december_1/puzzle_2.py
@@ -1,19 +1,19 @@
 # Calculate the number of increases and decreases.
 from puzzle_input import measurement_report, test_report
 
-
 TOTAL_EQUAL = 0
 TOTAL_INCREASE = 0
 TOTAL_DECREASE = 0
 
 
-def calc_diff(previous_measure: int,
-              current_measure: int,
-              total_equal: int = TOTAL_EQUAL,
-              total_increases: int = TOTAL_INCREASE,
-              total_decreases: int = TOTAL_DECREASE,
-              debug: bool = False
-              ) -> None:
+def calc_diff(
+    previous_measure: int,
+    current_measure: int,
+    total_equal: int = TOTAL_EQUAL,
+    total_increases: int = TOTAL_INCREASE,
+    total_decreases: int = TOTAL_DECREASE,
+    debug: bool = False,
+) -> None:
     global TOTAL_EQUAL
     global TOTAL_INCREASE
     global TOTAL_DECREASE
@@ -36,20 +36,24 @@ previous_window = []
 report = measurement_report
 
 for i in range(len(report)):
-    if i >= (len(report)-2):
+    if i >= (len(report) - 2):
         print("Last windows have been calculated.")
         break
     else:
         current_window = [
             report[i],
-            report[i+1],
-            report[i+2],
+            report[i + 1],
+            report[i + 2],
         ]
         if i == 0:
             None  # First entry knows no diff
         else:
-            calc_diff(current_measure=sum(current_window),
-                      previous_measure=sum(previous_window))
+            calc_diff(
+                current_measure=sum(current_window),
+                previous_measure=sum(previous_window),
+            )
         previous_window = current_window  # Save for next iteration
 
-print(f"Entries: {len(report)}, Number of increases: {TOTAL_INCREASE}, number of decreases: {TOTAL_DECREASE}, number of equals: {TOTAL_EQUAL}")
+print(
+    f"Entries: {len(report)}, Number of increases: {TOTAL_INCREASE}, number of decreases: {TOTAL_DECREASE}, number of equals: {TOTAL_EQUAL}"
+)
