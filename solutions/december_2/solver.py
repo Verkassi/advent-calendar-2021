9bd65081c1ca645aa9c1a756b222b5d68d558b59
Stefan Hendriks <stefan.hendriks@ns.nl>
2021-12-02 08:52:59 +0100

    add december 2nd solutions

---
+++ solutions/december_2/solver.py
@@ -0,0 +1,68 @@
+from puzzle_input import aoc_input, tst_input
+
+
+def solve_puzzle_1(in_list: list) -> None:
+    # Puzzle 1 logic:
+    # forward = + horizontal
+    # down    = + depth
+    # up      = - depth
+
+    cur_depth = 0
+    cur_horizontal = 0
+
+    for row in in_list:
+        row_sub = row.split(" ")
+        text_part = row_sub[0]
+        addition = int(row_sub[1])
+
+        if text_part == "down":
+            cur_depth = cur_depth + addition
+        elif text_part == "up":
+            addition = 0-addition  # Make negative
+            cur_depth = cur_depth + addition
+        elif text_part == "forward":
+            cur_horizontal = cur_horizontal + (addition)
+        else:
+            raise ValueError(f"Text is {text_part}, did't expect that")
+
+    print("---------------- PUZZLE ONE SOLUTION ----------------")
+    print(f"depth: {cur_depth}, horizontal: {cur_horizontal}")
+    print(f"Sum of depth and horizontal is: {cur_depth*cur_horizontal}")
+    print("-----------------------------------------------------")
+
+
+def solve_puzzle_2(in_list: list) -> None:
+    # Puzzle 2 logic:
+    # forward = + horizontal | depth * aim
+    # down    = + aim
+    # up      = - aim
+
+    cur_depth = 0
+    cur_horizontal = 0
+    cur_aim = 0
+
+    for row in in_list:
+        row_sub = row.split(" ")
+        text_part = row_sub[0]
+        addition = int(row_sub[1])
+
+        if text_part == "down":
+            cur_aim = cur_aim + addition
+        elif text_part == "up":
+            addition = 0-addition  # Make negative
+            cur_aim = cur_aim + addition
+        elif text_part == "forward":
+            cur_horizontal = cur_horizontal + (addition)
+            cur_depth = cur_depth + (cur_aim * addition)
+        else:
+            raise ValueError(f"Text is {text_part}, did't expect that")
+
+    print("---------------- PUZZLE TWO SOLUTION ----------------")
+    print(f"depth: {cur_depth}, horizontal: {cur_horizontal}, aim: {cur_aim}")
+    print(f"Sum of depth and horizontal is: {cur_depth*cur_horizontal}")
+    print("-----------------------------------------------------")
+
+
+if __name__ == "__main__":
+    solve_puzzle_1(aoc_input)
+    solve_puzzle_2(aoc_input)
