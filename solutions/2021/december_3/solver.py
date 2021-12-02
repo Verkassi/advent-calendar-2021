605b30ed242bbcaf81df4b4c5a2443ebf3988a3c
Stefan Hendriks <stefan.hendriks@ns.nl>
2021-12-02 17:12:16 +0100

    prepared december 3 2021

---
+++ solutions/2021/december_3/solver.py
@@ -0,0 +1,28 @@
+from puzzle_input import aoc_input, tst_input
+
+
+def solve_puzzle_1(in_list: list) -> None:
+    # Puzzle logic:
+    # 
+    # Awnser:
+    # 
+
+    print("---------------- PUZZLE ONE SOLUTION ----------------")
+    print(f"")
+    print("-----------------------------------------------------")
+
+
+def solve_puzzle_2(in_list: list) -> None:
+    # Puzzle logic:
+    # 
+    # Awnser:
+    # 
+
+    print("---------------- PUZZLE TWO SOLUTION ----------------")
+    print(f"")
+    print("-----------------------------------------------------")
+
+
+if __name__ == "__main__":
+    solve_puzzle_1(tst_input)
+    solve_puzzle_2(tst_input)
