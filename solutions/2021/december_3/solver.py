3723fd1aeb7a2ae788555967a9fb5a7b18ff9290
Stefan Hendriks <stefan.hendriks@ns.nl>
2021-12-04 07:29:48 +0100

    Solverd day3

--- solutions/2021/december_3/solver.py
+++ solutions/2021/december_3/solver.py
@@ -38,15 +38,58 @@ def solve_puzzle_1(in_list: list) -> None:
 
 def solve_puzzle_2(in_list: list) -> None:
     # Puzzle logic:
-    # 
+    # Flow:
+    # Start with full list from diagnostic report
+    # Consider just the first bit of each row
+    # Keep only numbers from the bit criteria:
+    #   ox_gen_rating = most common value in current bit position, if equals keep 1
+    #   co2_scrub_rating = least common value, if equals keep 0
+    # If there is only one number left, stop; this is the rating value
     # Answer:
-    # 
+    # Oxygen: 23
+    # CO2: 10
+    # LSR: 230
 
+    def find_commons(inputlist: list) -> int:
+        counter=collections.Counter(inputlist)
+        commons = counter.most_common()
+        # Check if entries are equal
+        if commons[0][1] == commons[1][1]:
+            return ("equal", None)
+        else:
+            return (commons[0][0], commons[1][0])
+            
+
+    def calc_matching_bitstring(input_list: list,
+                                equals_handler: str,
+                                calc_type: str) -> str:
+        row_width = len(input_list[0])
+        tmp_in_list = input_list
+
+        for y in range(row_width):
+            if len(tmp_in_list) == 1:
+                break
+            transposed_list=list(map(list, zip(*tmp_in_list)))
+            high_cur_mc, low_cur_mc = find_commons(transposed_list[y])
+            # handle equals per case
+            if high_cur_mc == "equal":
+                cur_mc = equals_handler
+            else:
+                if calc_type == "oxy":
+                    cur_mc = high_cur_mc
+                else:
+                    cur_mc = low_cur_mc
+            tmp_in_list = [ tmp_in_list[idx] for idx, entry in enumerate(tmp_in_list) if entry[y] == cur_mc ]
+        return tmp_in_list[0]
+
+    oxygen_generator_rating = int(calc_matching_bitstring(in_list, "1", calc_type="oxy"), 2)
+    co2_scrubber_rating = int(calc_matching_bitstring(in_list, "0", calc_type="co2"), 2)
+    life_support_rating=oxygen_generator_rating*co2_scrubber_rating
     print("---------------- PUZZLE TWO SOLUTION ----------------")
-    print(f"")
+    print(f"Oxygen: {oxygen_generator_rating}, CO2: {co2_scrubber_rating}, Life Support Rate: {life_support_rating}")
     print("-----------------------------------------------------")
 
 
 if __name__ == "__main__":
     solve_puzzle_1(aoc_input)
-    solve_puzzle_2(tst_input)
+    solve_puzzle_2(aoc_input)
