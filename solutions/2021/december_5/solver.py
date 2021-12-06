ed6e8a6ec3c824f0155f01453038f27e13e99a92
Verkassi <account@verkassi.online>
2021-12-06 23:01:16 +0100

    ran precommit

--- solutions/2021/december_5/solver.py
+++ solutions/2021/december_5/solver.py
@@ -1,19 +1,22 @@
-from puzzle_input import aoc_input, tst_input
 from copy import deepcopy
+
+from puzzle_input import aoc_input, tst_input
+
 
 def transform_input(raw_puzzle_in: str):
     # Format: x1,y1 -> x2,y2
     output = []
-    rows = raw_puzzle_in.replace(" ","").splitlines()
+    rows = raw_puzzle_in.replace(" ", "").splitlines()
     for row in rows:
         row = row.split("->")
         tmp_row = []
         for entry in row:
             entry = entry.split(",")
-            entry = tuple( int(item) for item in entry )
+            entry = tuple(int(item) for item in entry)
             tmp_row.append(entry)
         output.append(tmp_row)
     return output
+
 
 def create_base_grid(grid_size: int = 10):
     grid = []
@@ -24,12 +27,13 @@ def create_base_grid(grid_size: int = 10):
         grid.append(row)
     return grid
 
-def count_grid_numbers_higher(grid, gt_nr = 2):
+
+def count_grid_numbers_higher(grid, gt_nr=2):
     rtrn_cnt = 0
     for row in grid:
         for col in row:
             if col >= gt_nr:
-                rtrn_cnt+=1
+                rtrn_cnt += 1
     return rtrn_cnt
 
 
@@ -47,17 +51,18 @@ def solve_puzzle_1(puzzle_in, base_grid) -> None:
         x_coords = sorted([entry[0][0], entry[1][0]])
         y_coords = sorted([entry[0][1], entry[1][1]])
         if x_coords[0] == x_coords[1]:
-            for i in range(y_coords[0], y_coords[1]+1):
-                grid[i][x_coords[0]]+=1
+            for i in range(y_coords[0], y_coords[1] + 1):
+                grid[i][x_coords[0]] += 1
         elif y_coords[0] == y_coords[1]:
-            for i in range(x_coords[0], x_coords[1]+1):
-                grid[y_coords[0]][i]+=1
-        
+            for i in range(x_coords[0], x_coords[1] + 1):
+                grid[y_coords[0]][i] += 1
+
     nrs_higher_than_2 = count_grid_numbers_higher(grid, 2)
 
     print("---------------- PUZZLE ONE SOLUTION ----------------")
     print(f"Number of double overlaps: {nrs_higher_than_2}")
     print("-----------------------------------------------------")
+
 
 def solve_puzzle_2(puzzle_in, base_grid) -> None:
     # Puzzle logic:
@@ -72,11 +77,11 @@ def solve_puzzle_2(puzzle_in, base_grid) -> None:
         x_coords_srt = sorted(x_coords)
         y_coords_srt = sorted(y_coords)
         if x_coords_srt[0] == x_coords_srt[1]:
-            for i in range(y_coords_srt[0], y_coords_srt[1]+1):
-                grid[i][x_coords_srt[0]]+=1
+            for i in range(y_coords_srt[0], y_coords_srt[1] + 1):
+                grid[i][x_coords_srt[0]] += 1
         elif y_coords_srt[0] == y_coords_srt[1]:
-            for i in range(x_coords_srt[0], x_coords_srt[1]+1):
-                grid[y_coords_srt[0]][i]+=1
+            for i in range(x_coords_srt[0], x_coords_srt[1] + 1):
+                grid[y_coords_srt[0]][i] += 1
         else:
             # x1,y1 -> x2,y2 ==> x1,x2 | y1,y2
             # 8,0 -> 0,8 ==> 8,0 | 0,8 // Moves from right to left
@@ -84,20 +89,24 @@ def solve_puzzle_2(puzzle_in, base_grid) -> None:
             # 0,9 -> 2,9 ==> 0,2 | 9,9
 
             number_of_steps = 0
-            for i in range(x_coords_srt[0], x_coords_srt[1]+1):
-                number_of_steps+=1
+            for i in range(x_coords_srt[0], x_coords_srt[1] + 1):
+                number_of_steps += 1
 
-            x_entries = [ i for i in range(x_coords_srt[0], x_coords_srt[1]+1) ]
+            x_entries = [
+                i for i in range(x_coords_srt[0], x_coords_srt[1] + 1)
+            ]
             if x_coords[0] > x_coords[1]:
                 x_entries.reverse()
 
-            y_entries = [ i for i in range(y_coords_srt[0], y_coords_srt[1]+1) ]
+            y_entries = [
+                i for i in range(y_coords_srt[0], y_coords_srt[1] + 1)
+            ]
             if y_coords[0] > y_coords[1]:
                 y_entries.reverse()
 
             for i in range(number_of_steps):
-                grid[y_entries[i]][x_entries[i]]+=1
-    
+                grid[y_entries[i]][x_entries[i]] += 1
+
     nrs_higher_than_2 = count_grid_numbers_higher(grid, 2)
 
     print("---------------- PUZZLE ONE SOLUTION ----------------")
