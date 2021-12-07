24d1cca7aa18f0e67dd9bf621b3b4ab726a2e55f
Verkassi <account@verkassi.online>
2021-12-07 21:26:30 +0100

    revise puzzle input

--- solutions/template_solution/solver.py
+++ solutions/template_solution/solver.py
@@ -1,10 +1,10 @@
 from typing import Any, List
 
 from puzzle_input import aoc_input, tst_input
-
+from copy import deepcopy
 
 def transform_input(raw_puzzle_in: str) -> List[Any]:
-    return []
+    return raw_puzzle_in.split(",")
 
 
 def solve_puzzle_1(puzzle_in: list) -> None:
@@ -12,6 +12,8 @@ def solve_puzzle_1(puzzle_in: list) -> None:
     #
     # Answer:
     #
+
+    puzzle = deepcopy(puzzle_in)
 
     print("---------------- PUZZLE ONE SOLUTION ----------------")
     print(f"")
@@ -23,6 +25,8 @@ def solve_puzzle_2(puzzle_in: list) -> None:
     #
     # Answer:
     #
+
+    puzzle = deepcopy(puzzle_in)
 
     print("---------------- PUZZLE TWO SOLUTION ----------------")
     print(f"")
