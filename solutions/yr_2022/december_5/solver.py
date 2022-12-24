ef1559dabb0b699b56521efa5c57ab9903131c2d
Verkassi <account@verkassi.online>
2022-12-24 13:26:11 +0100

    december 5th

--- solutions/yr_2022/december_5/solver.py
+++ solutions/yr_2022/december_5/solver.py
@@ -20,38 +20,73 @@ def transform_input(raw_puzzle_in: str) -> List[Any]:
         current_stack = []
         for boxes in original_stack:
             starting_point = row + (row * 3)
-            print(boxes[starting_point : starting_point + 3])
-            current_stack.append(boxes[starting_point : starting_point + 3])
+            # print(boxes[starting_point : starting_point + 3])
+            box = boxes[starting_point : starting_point + 3]
+            if box != '   ':
+                current_stack.append(box)
+        current_stack.reverse()
         row_transpone[row + 1] = current_stack
 
     # Movements
     movements = puzzle_segments[1]
     movements = movements.splitlines()
-    # Int 1 == number of items
-    # Int 2 == From
-    # Int 3 == To
+    movement_lines = []
+    for movement in movements:
+        movement_separated = movement.split(" ")
+        actual_movement = [int(movement_separated[1]), int(movement_separated[3]), int(movement_separated[5])]
+        movement_lines.append(actual_movement)
 
-    return [row_transpone, movements]
+    return [row_transpone, movement_lines]
 
 
 def solve_puzzle_1(puzzle_in: list) -> None:
-    puzzle = deepcopy(puzzle_in)
+    stacks, movements = deepcopy(puzzle_in)
+
+    for movement in movements:
+        ammount = movement[0]
+        from_stack = movement[1]
+        to_stack = movement[2]
+        for i in range(ammount):
+            crate = stacks[from_stack].pop()
+            stacks[to_stack].append(crate)
+    
+    answer = []
+    for k, v in stacks.items():
+        answer.append(v.pop())
 
     print("---------------- PUZZLE ONE SOLUTION ----------------")
-    print(f"{puzzle}")
+    print(f"{''.join(answer).replace('[','').replace(']','')}")
     print("-----------------------------------------------------")
 
 
 def solve_puzzle_2(puzzle_in: list) -> None:
-    puzzle = deepcopy(puzzle_in)
+    stacks, movements = deepcopy(puzzle_in)
+
+    for movement in movements:
+        # number of crates
+        ammount = movement[0]
+
+        # from to orientation
+        from_stack = movement[1]
+        to_stack = movement[2]
+
+        # Moving
+        stacks[to_stack] += stacks[from_stack][-ammount:]
+        stacks[from_stack] = stacks[from_stack][:len(stacks[from_stack]) - ammount]
+
+    top_crates = []
+    for stack in stacks.values():
+        if len(stack) >= 1:
+            crate_value = f"{stack[-1].replace('[','').replace(']','')}"
+            top_crates.append(crate_value)
 
     print("---------------- PUZZLE TWO SOLUTION ----------------")
-    print(f"{puzzle}")
+    print(f"{''.join(top_crates)}")
     print("-----------------------------------------------------")
 
 
 if __name__ == "__main__":
-    transfrmd_inp_p1 = transform_input(tst_input)
+    transfrmd_inp_p1 = transform_input(aoc_input)
     solve_puzzle_1(transfrmd_inp_p1)
-    # transfrmd_inp_p2 = transform_input(tst_input)
-    # solve_puzzle_2(transfrmd_inp_p2)
+    transfrmd_inp_p2 = transform_input(aoc_input)
+    solve_puzzle_2(transfrmd_inp_p2)
