dd02819a27e0c3692f37c45b2027153ed6664cb1
Verkassi <sgjehendriks@gmail.com>
2023-12-28 09:50:21 +0100

    update template

--- solutions/template_solution/solver.py
+++ solutions/template_solution/solver.py
@@ -1,15 +1,23 @@
 from copy import deepcopy
 from pathlib import Path
 
-def read_input(is_test: bool = False):
+def read_input(is_test: bool = False, split_lines: bool = True):
     if is_test:
         input_file = Path("inputs/test.txt")
     else:
         input_file = Path("inputs/actual.txt")
 
     with open(input_file) as file:
-        content = file.read().splitlines()
+        if split_lines:
+            content = file.read().splitlines()
+        else:
+            content = file.read()
     return content
+
+def convert_input(is_test: bool = False, split_lines: bool = True):
+    input_data = read_input(is_test=is_test, split_lines=split_lines)
+
+    return input_data
 
 def solve_puzzle_1(puzzle_in: list) -> None:
     puzzle = deepcopy(puzzle_in)
@@ -28,7 +36,7 @@ def solve_puzzle_2(puzzle_in: list) -> None:
 
 
 if __name__ == "__main__":
-    transfrmd_inp_p1 = read_input(is_test=True)
+    transfrmd_inp_p1 = convert_input(is_test=True)
     solve_puzzle_1(transfrmd_inp_p1)
-    transfrmd_inp_p2 = read_input(is_test=True)
+    transfrmd_inp_p2 = convert_input(is_test=True)
     solve_puzzle_2(transfrmd_inp_p2)
